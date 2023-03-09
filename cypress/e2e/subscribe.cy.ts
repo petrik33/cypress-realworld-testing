b47ccb1149104190fc1594c004a8bc2c213120a5
Tsimafei Petrykevich <petrik3003@gmail.com>
2023-03-09 21:50:28 +0300

    test: subscribe form

---
+++ cypress/e2e/subscribe.cy.ts
@@ -0,0 +1,22 @@
+/// <reference types="cypress" />
+
+describe('Newsletter Subscribe Form', () => {
+  beforeEach(() => {
+    cy.visit("http://localhost:3000")
+      .wait(50);
+  })
+
+  it('allows users to subscribe to email list', () => {
+    cy.getByData("email-input")
+      .should('exist')
+      .type('tom@aol.com');
+
+    cy.getByData("submit-button")
+      .should('exist')
+      .click()
+
+    cy.getByData("success-message")
+      .should('exist')
+      .contains('tom@aol.com')
+  })
+})
