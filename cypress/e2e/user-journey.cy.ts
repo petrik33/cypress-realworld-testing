a8ab2127335744d24162456bd05d4217c0e16ef9
Tsimafei Petrykevich <petrik3003@gmail.com>
2023-03-10 19:07:59 +0300

    test: journey - 1st course

--- cypress/e2e/user-journey.cy.ts
+++ cypress/e2e/user-journey.cy.ts
@@ -1,5 +1,50 @@
 describe('User Journey', () => {
   it('a user can find a course on the home page and complete the course lessons', () => {
+    cy.visit('http://localhost:3000').wait(100)
+
+    cy.getByData('course-0')
+      .find('a')
+      .eq(3)
+      .click()
+
+    cy.location('pathname')
+      .should('eq', '/testing-your-first-application')
+
+    cy.getByData("next-lesson-button")
+      .click()
+
+    cy.location('pathname')
+      .should('eq', '/testing-your-first-application/app-install-and-overview')
+
+    cy.getByData("challenge-answer-0")
+      .click()
+
+    cy.getByData("next-lesson-button")
+      .should('exist')
+      .click()
+
+    cy.location('pathname')
+      .should('eq', '/testing-your-first-application/installing-cypress-and-writing-our-first-test')
+
+    cy.getByData("challenge-answer-0")
+      .click()
+
+    cy.getByData("next-lesson-button")
+      .should('exist')
+      .click()
+
+    cy.location('pathname')
+      .should('eq', '/testing-your-first-application/setting-up-data-before-each-test')
+
+    cy.getByData("challenge-answer-0")
+      .click()
+
+    cy.getByData("next-lesson-button")
+      .should('exist')
+      .click()
+
+    cy.location('pathname')
+      .should('eq', '/')
     
   })
 })
